7de782648dead493963cc6f36cd92d33e4f5b120
vr647048-max <vr647048@gmail.com>
2026-08-24 15:31:50 +0530

    Add TherapyOnWay service worker for installable mobile app

---
+++ service-worker.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'therapy-on-way-v1';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './config.js',
+  './track.html',
+  './admin.html',
+  './assets/logo.svg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+
+self.addEventListener('fetch', event => {
+  const request = event.request;
+  if (request.method !== 'GET') return;
+  const url = new URL(request.url);
+  if (url.origin !== self.location.origin) return;
+  event.respondWith(
+    fetch(request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(request, copy));
+      return response;
+    }).catch(() => caches.match(request).then(cached => cached || caches.match('./index.html')))
+  );
+});
